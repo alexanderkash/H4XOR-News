bb4724fb7a99135abd624f5dbd36cd3552848011
alexanderkash <83585555+alexanderkash@users.noreply.github.com>
2021-06-09 12:17:12 +0300

    Initial commit

---
+++ H4X0R News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4X0R News
+//
+//  Created by Alexander Kogalovsky on 4/29/21.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
---
+++ H4X0R News/Views/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Alexander Kogalovsky on 4/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            if #available(macCatalyst 14.0, *) {
+                List(networkManager.posts) { post in
+                    NavigationLink(
+                        destination: DetailView(url: post.url)) {
+                        HStack {
+                            Text(String(post.points))
+                            Text(post.title)
+                        }
+                    }
+                }
+                .navigationTitle("H4X0R NEWS")
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Hola")
+//]
+
